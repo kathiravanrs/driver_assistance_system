426d2531d5447599bd15d2bb65627cd52f430a6b
kathiravanrs <kathiravanrs@gmail.com>
2021-04-24 09:13:36 +0530

    Login Screen Added

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:driver_assistance_system/auth.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({this.auth, this.onSignOut});
+  final BaseAuth auth;
+  final VoidCallback onSignOut;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final databaseReference = FirebaseDatabase.instance.reference();
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+
+    void _signOut() async {
+      try {
+        await widget.auth.signOut();
+        widget.onSignOut();
+      } catch (e) {
+        print(e);
+      }
+
+    }
+
+    return new Scaffold(
+      appBar: new AppBar(
         title: Text('Driver Assistance System - Test'),
+        actions: <Widget>[
+          new TextButton(
+              onPressed: _signOut,
+              child: new Text('Logout', style: new TextStyle(fontSize: 17.0, color: Colors.white))
+          )
+        ],
       ),
       body: Center(
         child: Column(
@@ -55,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).pushNamed('/road');
-                  },
+                },
                 child: Text(
                   'Road Condition',
                   style: TextStyle(fontSize: 36),
